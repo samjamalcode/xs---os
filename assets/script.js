bdaac3bfe7d80f6e47e02d05bdc2a9aef0921022
samjamalcode <>
2023-12-05 15:43:44 +0000

    Creates 9 game cells using 'loop' iteration

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
     let gameBoard = ["", "", "", "", "", "", "", "", ""];
     let computerTurnEnabled = false;
     let playerMoves = 0;
+
+    // Create the game board by adding click event listeners to each cell
+    for (let i = 0; i < 9; i++) {
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.addEventListener("click", () => handleCellClick(i));
+        board.appendChild(cell);
+    }
